52fd34978cda54b0927bf58d522e369624886bc1
songyuo <1027241191@qq.com>
2022-02-25 23:36:40 +0800

    Initial commit

---
+++ src/main/java/com/example/myFirstOwnSpringBoot/MyFirstOwnSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.myFirstOwnSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstOwnSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstOwnSpringBootApplication.class, args);
+	}
+
+}
